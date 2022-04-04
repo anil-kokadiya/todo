d5f1ad0c284d61df215878734bd8ddd997ea4666
Anil Kokadiya <anil.kokadiya@streebo.com>
2022-04-04 12:15:32 +0530

    Removed Unused Imports

--- src/app/components/todo-view/todo-view.component.ts
+++ src/app/components/todo-view/todo-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Todo } from '../todo';
 import { TodoService } from 'src/app/service/todo.service';
 import { Router } from '@angular/router';
